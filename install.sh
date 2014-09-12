0a364fbd54791f71fd8139ea650012e42e8c3641
Ferran Basora <fcsonline@gmail.com>
2014-09-12 10:30:56 +0200

    Install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# BASH
+ln -sf ${PWD}/bash/bashrc ~/.bashrc
+ln -sf ${PWD}/bash/bash_profile ~/.bash_profile
+ln -sf ${PWD}/bash/bash_aliases ~/.bash_aliases
+
+# GIT
+ln -sf ${PWD}/git/gitconfig ~/.gitconfig
