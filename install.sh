317495cac25e0fa5866589816061333d12ff75c6
Ferran Basora <fcsonline@gmail.com>
2014-09-12 11:22:41 +0200

    Added FZF

--- install.sh
+++ install.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+message ()
+{
+  RCOLOR=$(( ( RANDOM % 6 )  + 1 ))
+  echo -e "Installing \033[1;3${RCOLOR}m$1\033[0m scripts..."
+}
+
 # BASH
+message "Bash"
 ln -sf ${PWD}/bash/bashrc ~/.bashrc
 ln -sf ${PWD}/bash/bash_profile ~/.bash_profile
 ln -sf ${PWD}/bash/bash_aliases ~/.bash_aliases
 
+# BASH FZF
+message "FZF"
+rm -rf ~/.fzf
+git clone -q https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install > /dev/null
+
 # GIT
+message "Git"
 ln -sf ${PWD}/git/gitconfig ~/.gitconfig
+
+# Execute it immediately
+source ~/.bashrc
