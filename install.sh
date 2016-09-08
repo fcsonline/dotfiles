5f8882be5e5c0c394dc385b822acd701d8a7b2e2
Ferran Basora <fcsonline@gmail.com>
2016-09-08 17:20:12 +0200

    Shell check

--- install.sh
+++ install.sh
@@ -8,14 +8,14 @@ message ()
 
 # TMUX
 message "Tmux"
-ln -snf ${PWD}/tmux/tmux.conf ~/.tmux.conf
+ln -snf "${PWD}/tmux/tmux.conf" ~/.tmux.conf
 
 # BASH
 message "Bash"
-ln -snf ${PWD}/bash/bashrc ~/.bashrc
-ln -snf ${PWD}/bash/bash_profile ~/.bash_profile
-ln -snf ${PWD}/bash/bash_aliases ~/.bash_aliases
-ln -snf ${PWD}/bash/inputrc ~/.inputrc
+ln -snf "${PWD}/bash/bashrc" ~/.bashrc
+ln -snf "${PWD}/bash/bash_profile" ~/.bash_profile
+ln -snf "${PWD}/bash/bash_aliases" ~/.bash_aliases
+ln -snf "${PWD}/bash/inputrc" ~/.inputrc
 
 # BASH FZF
 message "FZF"
@@ -25,16 +25,16 @@ yes | ~/.fzf/install > /dev/null 2>&1
 
 # GIT
 message "Git"
-ln -sf ${PWD}/git/gitconfig ~/.gitconfig
+ln -sf "${PWD}/git/gitconfig" ~/.gitconfig
 
 # VIM
 message "Vim"
-ln -snf ${PWD}/vimrc ~/.vimrc
-ln -snf ${PWD}/vim ~/.vim
+ln -snf "${PWD}/vimrc" ~/.vimrc
+ln -snf "${PWD}/vim" ~/.vim
 mkdir -p ~/.vim/{tmpdir,undodir}
 chmod 700 ~/.vim/{tmpdir,undodir}
 
-pushd ${PWD}
+pushd "${PWD}"
   git submodule init > /dev/null 2>&1
   git submodule update > /dev/null 2>&1
 popd
