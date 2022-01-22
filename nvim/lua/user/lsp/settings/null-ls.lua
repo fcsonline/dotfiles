e9c54770e9259088d35f4c63c7ee0ba084c88dfc
Ferran Basora <fcsonline@gmail.com>
2022-01-22 10:41:51 +0000

    Tune rubocop null-ls

--- nvim/lua/user/lsp/settings/null-ls.lua
+++ nvim/lua/user/lsp/settings/null-ls.lua
@@ -9,22 +9,32 @@ local diagnostics = null_ls.builtins.diagnostics
 local completion = null_ls.builtins.completion
 local null_ls_sources = {}
 
--- formatting
-table.insert(null_ls_sources, formatting.rubocop)
+local exists_local_file = function (local_path)
+  return vim.fn.filereadable(vim.fn.getcwd() .. '/' .. local_path) == 1
+end
+
+-- Rubocop doesn't have command_resolver like eslint, so we have to do this to
+-- take in account the bash configuration to run rubocop
+if exists_local_file('.rubocop.yml') and exists_local_file('Gemfile') then
+  table.insert(null_ls_sources, formatting.rubocop.with({
+    command = 'bash',
+    args = { '-c', 'rubocop --auto-correct -f quiet --stderr --stdin $FILENAME' }
+  }))
+  table.insert(null_ls_sources, diagnostics.rubocop.with({
+    command = 'bash',
+    args = { '-c', 'rubocop -f json $FILENAME' }
+  }))
+end
+
 table.insert(null_ls_sources, formatting.eslint)
-table.insert(null_ls_sources, formatting.rustywind) -- Tailwind organizer
-
--- diagnostics
-table.insert(null_ls_sources, diagnostics.rubocop)
 table.insert(null_ls_sources, diagnostics.eslint)
+table.insert(null_ls_sources, formatting.rustywind) -- Tailwind organizer
 table.insert(null_ls_sources, diagnostics.shellcheck)
-
--- completion
 table.insert(null_ls_sources, completion.spell.with({ filetypes = { "markdown" } }))
 
 if #null_ls_sources ~= 0 then
   null_ls.setup({
-    debug = false,
+    debug = true,
     sources = null_ls_sources,
     on_attach = function(client)
       if client.resolved_capabilities.document_formatting then
@@ -38,3 +48,4 @@ if #null_ls_sources ~= 0 then
     end,
   })
 end
+
